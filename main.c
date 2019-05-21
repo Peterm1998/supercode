d97132561905fbb2d03edb5a7f489aa97444fde3
Peterm1998 <40659074+Peterm1998@users.noreply.github.com>
2019-05-21 09:35:38 +0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main()
+{
+	int a;
+	int b=4;
+	int c=8;
+	int d=2;
+	int e=4;
+	int f=2;
+	a=b+c/d+e*f
+	printf("The value of a is=%d\n",a);
+return 0;
+}
