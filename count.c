bc061585faac9dbcd199d75f62c671e87f3519a8
firefox14-ops <72151385+firefox14-ops@users.noreply.github.com>
2020-09-30 21:59:58 +0530

    Update count.c

--- count.c
+++ count.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-int main(){
+void main(){
 int n[10];
 int i,j;
   int q;
@@ -7,5 +7,4 @@ for(i=0;i<10;i++){
 n[i]=i+100;}
 for(j=0;j<10;j++){
 printf("element[%d]=%d\n",j,n[j];}
-return 0;
 }
